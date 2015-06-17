fb33ae34ae9eb792494794327e3d6c2f7db5ab67
Braiden Vasco <braiden-vasco@mailtor.net>
2015-06-17 09:13:57 +0500

    Implement Lita::Adapters::Vkontakte#initialize

--- lib/lita/adapters/vkontakte.rb
+++ lib/lita/adapters/vkontakte.rb
@@ -1,5 +1,7 @@
 require 'lita'
 
+require 'vkontakte_api'
+
 ##
 # Lita module.
 #
@@ -12,9 +14,26 @@ module Lita
     # VKontakte adapter for the Lita chat bot.
     #
     class Vkontakte < Adapter
+      API_VERSION = '5.34'
+
+      REDIRECT_URI = 'https:/oauth.vk.com/blank.html'
+
       config :client_id,     type: String, required: true
       config :client_secret, type: String, required: true
       config :access_token,  type: String, required: true
+
+      def initialize(robot)
+        super
+
+        VkontakteApi.configure do |vk|
+          vk.app_id       = config.client_id
+          vk.app_secret   = config.client_secret
+          vk.redirect_uri = REDIRECT_URI
+          vk.api_version  = API_VERSION
+        end
+
+        @vk = VkontakteApi::Client.new(config.access_token)
+      end
     end
 
     Lita.register_adapter(:vkontakte, Vkontakte)
